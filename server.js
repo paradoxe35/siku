648a6d568eb505bf80a4e2494967e9c0c8de4e49
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2020-10-22 03:58:50 +0200

    P

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ require('laravel-echo-server').run({
     authHost: env.APP_URL,
     devMode: env.APP_DEBUG,
     database: "redis",
-    devMode: process.env.ECHO_DEV_MODE,
-    host: process.env.ECHO_HOST,
-    protocol: process.env.ECHO_PROTOCOL,
     port: process.env.ECHO_PORT,
     databaseConfig: {
         redis: {
