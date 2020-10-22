8eba4ed73632ad9c674c261250165d10b9a05b69
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2020-10-22 03:11:51 +0200

    P

--- server.js
+++ server.js
@@ -12,5 +12,7 @@ require('laravel-echo-server').run({
             port: env.REDIS_PORT,
             password: env.REDIS_PASSWORD
         }
-    }
+    },
+    "verifyAuthPath": true,
+    "verifyAuthServer": true
 });
