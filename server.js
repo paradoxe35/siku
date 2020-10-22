61fb9020cfcfcbadd083322d33dc2881be437b42
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2020-10-22 02:20:53 +0200

    P

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const env = process.env;
 require('laravel-echo-server').run({
     authHost: env.APP_URL,
     devMode: env.APP_DEBUG,
+    authEndpoint: "/broadcasting/auth",
     database: "redis",
     databaseConfig: {
         redis: {
