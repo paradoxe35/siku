a4cbd646da60f0a22bc4b55c3da33ee768a4f402
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2020-11-07 19:36:20 +0200

    fix card table component responsive

--- resources/views/components/card-table.blade.php
+++ resources/views/components/card-table.blade.php
@@ -1,17 +1,19 @@
 <div class="card shadow-sm">
     <div class="card-body">
-        <table class="table align-items-center table-hover">
-            <thead class="thead-light">
-                <tr>
-                    @foreach ($ths as $th)
-                    <th scope="col" {{ $sort ? 'class="sort" data-sort="name"' : '' }}>{{ __($th) }}</th>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody class="list">
-                {{ $slot }}
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table align-items-center table-hover">
+                <thead class="thead-light">
+                    <tr>
+                        @foreach ($ths as $th)
+                        <th scope="col" {{ $sort ? 'class="sort" data-sort="name"' : '' }}>{{ __($th) }}</th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody class="list">
+                    {{ $slot }}
+                </tbody>
+            </table>
+        </div>
         <div class="mt-3 d-flex justify-content-end">
             {{ $paginate ? $paginate->withQueryString()->links() : '' }}
         </div>
