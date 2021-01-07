e2b1ead1b11ce08d630c06dcbe4c44f871effbea
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2021-01-07 04:10:56 +0200

    iframe reziser static id

--- resources/views/components/livewire/frame.blade.php
+++ resources/views/components/livewire/frame.blade.php
@@ -1,4 +1,4 @@
-<iframe style="width: 100%;" id="{{ Str::random(20) }}" scrolling="no"
+<iframe style="width: 100%;" id="livewire-frame-datatable" scrolling="no"
     src="{{ route('livewire', array_merge($attributes->getAttributes(), ['component' => $ref]), false) }}"
     frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen
     onload="var t = this; if(resizeIframe){resizeIframe(t)}else{window.onload=function(){resizeIframe(t)}}"></iframe>
